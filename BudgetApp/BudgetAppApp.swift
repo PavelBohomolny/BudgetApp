70791f16653bfc635addc4deefe2eea3327fe96d
Pavel Bohomolnyi <62807847+PavelBohomolny@users.noreply.github.com>
2024-07-04 17:51:44 +0200

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Pavel Bohomolnyi on 04/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
